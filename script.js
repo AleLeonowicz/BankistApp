5017fff13ac6f052c0f9bb8fa88073a74d2b98cd
AleLeonowicz <aleks.leonowicz@gmail.com>
2022-01-07 14:31:57 +0100

    Created createUsername function

--- script.js
+++ script.js
@@ -78,6 +78,27 @@ const displayMovements = function (movements) {
 };
 displayMovements(account1.movements);
 
+const createUsername = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(function (word) {
+        return word[0];
+      })
+      .join('');
+  });
+};
+
+createUsername(accounts);
+console.log(accounts);
+// const user = 'Steven Thomas Williams'; // stw
+// const username1 = user.toLowerCase().split(' ');
+// const username2 = username.map(function (word) {
+//   return word[0];
+// });
+
+// console.log(username2);
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
