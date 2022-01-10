520633967956b3fbc692ef8c903d6364b21df660
AleLeonowicz <aleks.leonowicz@gmail.com>
2022-01-10 12:54:57 +0100

    Created calcDisplayBallance function

--- script.js
+++ script.js
@@ -70,13 +70,45 @@ const displayMovements = function (movements) {
       <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-      <div class="movements__value">${mov}</div>
+      <div class="movements__value">${mov}€</div>
     </div>`;
 
     containerMovements.insertAdjacentHTML('afterbegin', html);
   });
 };
 displayMovements(account1.movements);
+
+const calcDisplayBallance = function (movements) {
+  const balance = movements.reduce(function (accumulator, mov) {
+    return accumulator + mov;
+  }, 0);
+  labelBalance.textContent = `${balance}€`;
+};
+calcDisplayBallance(account1.movements);
+
+const calcDisplaySummary = function (movements) {
+  const incomes = movements
+    .filter(mov => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${incomes}€`;
+
+  const outcomes = movements
+    .filter(mov => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${Math.abs(outcomes)}€`;
+
+  const interests = movements
+    .filter(mov => mov > 0)
+    .map(deposit => (deposit * 1.2) / 100)
+    .filter((int, i, arr) => {
+      console.log(arr);
+      return int >= 1;
+    })
+    .reduce((acc, int) => acc + int, 0);
+  labelSumInterest.textContent = `${interests}€`;
+};
+
+calcDisplaySummary(account1.movements);
 
 const createUsername = function (accs) {
   accs.forEach(function (acc) {
@@ -89,16 +121,9 @@ const createUsername = function (accs) {
       .join('');
   });
 };
-
 createUsername(accounts);
 console.log(accounts);
-// const user = 'Steven Thomas Williams'; // stw
-// const username1 = user.toLowerCase().split(' ');
-// const username2 = username.map(function (word) {
-//   return word[0];
-// });
 
-// console.log(username2);
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -229,8 +254,24 @@ const checkDogs = function (arr1, arr2) {
 checkDogs(juliasList1, katesList1);
 checkDogs(juliasList2, katesList2);
 
-*/
+// CODING CHALLENGE 2
+const calcAvarageHumanAge = function (ages) {
+  const humanAges = ages.map(function (dogAge) {
+    return dogAge <= 2 ? 2 * dogAge : 16 + dogAge * 4;
+  });
+  const adults = humanAges.filter(function (humanAge) {
+    return humanAge > 18;
+  });
+  const sum = adults.reduce(function (acc, cur) {
+    return acc + cur;
+  }, 0);
+  return sum / adults.length;
+};
 
+console.log(calcAvarageHumanAge([5, 2, 4, 1, 15, 8, 3]));
+console.log(calcAvarageHumanAge([16, 6, 10, 5, 6, 1, 4]));
+
+*/
 /*
 const eurToUsd = 1.1;
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
@@ -249,3 +290,51 @@ for (const mov of movements) movementsUsdfor.push(mov * eurToUsd);
 console.log(movementsUsdfor);
 
 */
+
+/*
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const deposits = movements.filter(function (mov) {
+  return mov > 0;
+});
+console.log(deposits);
+
+const withdrawals = movements.filter(function (mov) {
+  return mov < 0;
+});
+console.log(withdrawals);
+
+// accumulator -> SNOWBALL
+const balance = movements.reduce(function (acc, cur, i, arr) {
+  console.log(`Iteration ${i}: ${acc}`);
+  return acc + cur;
+}, 0);
+
+console.log(balance);
+
+// Maximum value
+const max = movements.reduce(function (acc, mov) {
+  if (acc > mov) {
+    return acc;
+  } else {
+    return mov;
+  }
+}, movements[0]);
+
+console.log(max);
+*/
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const eurToUsd = 1.1;
+
+// PIPELINE
+const totalDepositSD = movements
+  .filter(mov => mov > 0)
+  .map((mov, i, arr) => {
+    // console.log(arr);
+    return mov * eurToUsd;
+  })
+  // .map(mov => mov * eurToUsd)
+  .reduce((acc, mov) => acc + mov, 0);
+
+console.log(totalDepositSD);
